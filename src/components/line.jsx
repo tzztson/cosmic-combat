205368e26ad23c5b2eec21a5a45023eb0942f7fd
BigBen3918 <benjamin199551@gmail.com>
2022-11-05 13:32:01 -0700

    initial commit

---
+++ src/components/line.jsx
@@ -0,0 +1,9 @@
+const Line = () => {
+    return (
+        <div className="divide_line">
+            <img src={require("../assets/cosmic-line.png")} alt="" />
+        </div>
+    );
+};
+
+export default Line;
--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -1,9 +1,34 @@
+import Line from "../components/line";
+
 const Main = () => {
     return (
         <div className="main">
             <div className="section1">
                 <img src={require("../assets/cosmic-logo.png")} />
             </div>
+            <Line />
+            <div className="section2">
+                <div className="title">
+                    <p>GAME PLAY</p>
+                </div>
+                <div className="content">
+                    <div>
+                        <img
+                            src={require("../assets/cosmic-inventory-1.png")}
+                            alt=""
+                        />
+                    </div>
+                    <div className="">
+                        <div className="requirements">
+                            <h3>Requirements</h3>
+                            <p>2Players</p>
+                            <p>7 tile for each player</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="section3"></div>
+            <div className="bottom-height"></div>
         </div>
     );
 };
