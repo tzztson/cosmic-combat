b266d3ab0314d99f5bf234e0519ece3159adef3b
BigBen3918 <benjamin199551@gmail.com>
2022-11-05 12:17:24 -0700

    initial commit

---
+++ src/pages/main.jsx
@@ -0,0 +1,11 @@
+const Main = () => {
+    return (
+        <div className="main">
+            <div className="section1">
+                <img src={require("../assets/cosmic-logo.png")} />
+            </div>
+        </div>
+    );
+};
+
+export default Main;
